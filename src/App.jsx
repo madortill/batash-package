4e830708df877613ac301217c8450411a51e7d76
madortill <mador.till@gmail.com>
2026-02-03 12:31:04 +0200

    3.2 lunch

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './style/App.css'
+import til from "./assets/images/til.svg";
+import bahad6 from "./assets/images/bahad6.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showAbout, setShowAbout] = useState(false);
+  const [infoSymbol, setInfoSymbol] = useState("i");
+  const toggleAbout = () => {
+    setShowAbout((prev) => !prev);
+    setInfoSymbol((prev) => (prev === "i" ? "x" : "i"));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="symbols">
+        <img src={bahad6} alt="bahad6" className="bahad6" />
+        <img src={til} alt="til" className="til" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+       <div onClick={toggleAbout} className="aboutBtnContainer">
+        <button className="about-btn">
+          {infoSymbol}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save gfg to test HMR
-        </p>
+        <p>אודות</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={`div-about ${showAbout ? "fade-in" : "fade-out"}`}>
+        {showAbout && (
+          <>
+            <h3 className="list-text-about">מפתחת ראשית:</h3>
+            <p className="list-text-about">רב"ט גילי נחום</p>
+            <h3 className="list-text-about">גרפיקה:</h3>
+            <p className="list-text-about">רב"ט גילי נחום</p>
+            <h3 className="list-text-about">מומחי תוכן:</h3>
+            <p className="list-text-about">רס"ל עדן מאיר</p>
+            <p className="list-text-about">רנ"ג יוסי אלוני</p>
+            <h3 className="list-text-about">רמ"ד טי"ל:</h3>
+            <p className="list-text-about">רס"מ עדן בן חמו</p>
+            <h3 className="list-text-about">גרסה:</h3>
+            <p className="list-text-about">דצמבר 2025</p>
+          </>
+        )}
+      </div>
     </>
   )
 }
